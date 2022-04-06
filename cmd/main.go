d99a2c9a86cf2b8744bc4ac27504683f633a4c4c
diagmatrix <manuelgbal1@gmail.com>
2022-04-06 16:33:48 +0200

    Perlin noise but it does not work as i want it to :(

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	gomap "github.com/diagmatrix/gomap/internal"
 )
 
 func main() {
-	var h gomap.HeightMap = *gomap.NewHeightMap(1000, 1000)
-	gomap.SaveHeightMap("./test/test.png", &h)
+	var h gomap.HeightMap = *gomap.NewHeightMapRN(1024, 1024)
+	gomap.SaveHeightMap("./test/testRN.png", &h)
+	h = *gomap.NewHeightMapPN(1024, 1024, 2, 2, 1, rand.NewSource(time.Now().Unix()))
+	gomap.SaveHeightMap("./test/testPN.png", &h)
 }
--- internal/heightmap.go
+++ internal/heightmap.go
@@ -7,6 +7,8 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/diagmatrix/gomap/internal/pnoise"
 )
 
 // Heightmap
@@ -15,6 +17,10 @@ type HeightMap struct {
 	Rect image.Rectangle // Bounds
 }
 
+var _time float64 = 0
+
+// ----------------------------------------------------------------------------
+// Image interface functions
 func (h *HeightMap) At(x, y int) color.Color {
 	out := x < h.Rect.Min.X || x >= h.Rect.Dx() || y < h.Rect.Min.Y || y >= h.Rect.Dy()
 	if out {
@@ -32,13 +38,15 @@ func (h *HeightMap) ColorModel() color.Model {
 	return color.GrayModel
 }
 
-func NewHeightMap(w, h int) *HeightMap {
-	rand.Seed(time.Now().Unix())
+// ----------------------------------------------------------------------------
+// Heightmap generator functions
+func NewHeightMapRN(w, h int) *HeightMap {
+	seed()
 	matrix := make([][]uint8, w)
 	for i := range matrix {
 		matrix[i] = make([]uint8, h)
 		for j := range matrix[i] {
-			matrix[i][j] = uint8(rand.Int31n(256))
+			matrix[i][j] = uint8(rand.Int31n(65535))
 		}
 	}
 	return &HeightMap{
@@ -47,6 +55,24 @@ func NewHeightMap(w, h int) *HeightMap {
 	}
 }
 
+func NewHeightMapPN(w, h int, a, b float64, o int32, src rand.Source) *HeightMap {
+	nm, min, max := pnoise.NoiseMap(w, h, a, b, o, src)
+	matrix := make([][]uint8, w)
+	for i := 0; i < w; i++ {
+		matrix[i] = make([]uint8, h)
+		for j := 0; j < h; j++ {
+			matrix[i][j] = toGrayscale(nm[i][j], min, max)
+		}
+	}
+
+	return &HeightMap{
+		Pix:  matrix,
+		Rect: image.Rect(0, 0, w, h),
+	}
+}
+
+// ----------------------------------------------------------------------------
+// Other functions
 func SaveHeightMap(s string, h *HeightMap) error {
 	f, err := os.Create(s)
 	if err != nil {
@@ -60,4 +86,10 @@ func SaveHeightMap(s string, h *HeightMap) error {
 		return err
 	}
 	return nil
+}
+func seed() {
+	rand.Seed(time.Now().Unix())
+}
+func toGrayscale(t, min, max float64) uint8 {
+	return uint8(((t - min) / (max - min)) * 255)
 }
