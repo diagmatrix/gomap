7473a09f0c5d1460ce15fc3b567902d05a9125d6
diagmatrix <manuelgbal1@gmail.com>
2022-04-07 20:45:17 +0200

    Added KNN smoothing and it works (you have to pass it multiple times tho for it to be useful)

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,23 @@ import (
 	"fmt"
 
 	gomap "github.com/diagmatrix/gomap/internal"
+	"github.com/diagmatrix/gomap/internal/smoothing"
 )
 
 func main() {
 	// 8 octaves 128 amplitude
-	for i := 1; i < 9; i++ {
-		for j := 255; j > 16; j = j / 2 {
-			fmt.Println("Octaves: ", i, "\tAmplitude: ", j)
-			h := *gomap.NewHeightMapDS(512, uint8(i), uint8(j))
-			gomap.SaveHeightMap("./test/test("+fmt.Sprint(i)+"-"+fmt.Sprint(j)+").png", &h)
-			h = *gomap.NewHeightMapDSS(512, uint8(i), uint8(j))
-			gomap.SaveHeightMap("./test/testS("+fmt.Sprint(i)+"-"+fmt.Sprint(j)+").png", &h)
+	for i := 8; i <= 8; i++ {
+		for j := 127; j >= 127; j = j / 2 {
+			h := *gomap.NewHeightMapDS(256, uint8(i), uint8(j))
+			gomap.SaveHeightMap("./test/testO"+fmt.Sprint(i)+"A"+fmt.Sprint(j)+".png", &h)
+			nh := h
+			var count int = 0
+			for k := 10; k > 1; k-- {
+				count++
+				fmt.Println("Octaves: ", i, "\tAmplitude: ", j, "\t Kernel size: ", 5, "\tPass: ", count)
+				nh.Pix = smoothing.SmoothKNN(&nh.Pix, 5)
+				gomap.SaveHeightMap("./test/testO"+fmt.Sprint(i)+"A"+fmt.Sprint(j)+"K"+fmt.Sprint(5)+"P"+fmt.Sprint(count)+".png", &nh)
+			}
 		}
 	}
 
---
+++ internal/smoothing/smooth.go
@@ -0,0 +1,78 @@
+package smoothing
+
+import (
+	"math"
+)
+
+// Smoothing KNN
+func SmoothKNN(hm *[][]uint8, k int) [][]uint8 {
+	// Kernel heightmap
+	var w, h int = len(*hm), len((*hm)[0])
+	var res [][]float64 = make([][]float64, w)
+	for i := range res {
+		res[i] = make([]float64, h)
+	}
+	var rad int = k / 2
+	var dmax float64 = float64(rad) * math.Sqrt2
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			// Kernel creation (k x k)
+			var xmin, xmax int = max(0, x-rad), min(w-1, x+rad)
+			var ymin, ymax int = max(0, y-rad), min(h-1, y+rad)
+			var sum float64 = 0
+			var count int = 0
+			for i := xmin; i <= xmax; i++ {
+				for j := ymin; j <= ymax; j++ {
+					// Weight depends on distance
+					sum += (dist(x, y, i, j) / dmax) * float64((*hm)[i][j])
+					count++
+				}
+			}
+			var kernel_avg float64 = sum / float64(count)
+			res[x][y] = kernel_avg
+		}
+	}
+	// Result heightmap
+	var nhm [][]uint8 = make([][]uint8, w)
+	for i := range nhm {
+		nhm[i] = make([]uint8, h)
+		for j := range nhm[i] {
+			var val uint8 = uint8(res[i][j] * 1.6) // It's a hack
+			nhm[i][j] = uint8(val)
+		}
+	}
+
+	return nhm
+}
+
+/*
+TODO:
+	- Implement other smoothing functions
+	- Fix the hack
+*/
+
+// ----------------------------------------------------------------------------
+// Auxiliary functions
+
+// Min
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// Max
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// Normalized distance
+func dist(x0, y0, x1, y1 int) float64 {
+	return math.Sqrt(float64(x0-x1)*float64(x0-x1) + float64(y0-y1)*float64(y0-y1))
+}
